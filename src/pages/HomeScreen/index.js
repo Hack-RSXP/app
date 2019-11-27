4e75b2bff6496a10e3769d130a3e5ad0378cfcd7
Joao Pedro <joaopedrobeckland@gmail.com>
2019-11-27 07:49:27 -0300

    App

---
+++ src/pages/HomeScreen/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import {Container, ButtonDiv} from './styles';
+import Background from '../../components/Background';
+
+export default function HomeScreen() {
+  return (
+    <Background>
+      <Container>
+        <ButtonDiv />
+      </Container>
+    </Background>
+  );
+}
